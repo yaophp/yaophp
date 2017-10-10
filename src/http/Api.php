6a1ec74d481d120141a9ecf4db314bfd5252e8e2
yao <yao>
2017-10-10 14:07:39 +0800

    add api request and respone and add success and error

--- src/http/Api.php
+++ src/http/Api.php
@@ -1,10 +1,42 @@
 <?php
 namespace yaophp\http;
 
+use yaophp\http\Request;
+use yaophp\http\Response;
 use yaophp\exception\HttpException;
 
 class Api
 {
+    protected $request;
+    protected $response;
+
+    public function __construct(Request $request, Response $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function success($data='')
+    {
+        $result = [
+            'code' => 1,
+            'data' => $data
+        ];
+        $this->response->withJson($result);
+        $this->response->output();
+    }
+
+    public function error($data='')
+    {
+        $result = [
+            'code' => 0,
+            'data' => $data
+        ];
+        $this->response->withJson($result);
+        $this->response->output();
+        exit;
+    }
+
     public function get()
     {
         throw new \HttpException('http request method not allow');
